74d04f2cdb52074fbfed8dce93289a6d05dacea1
cmdr2 <secondary.cmdr2@gmail.com>
2022-09-01 20:38:36 +0530

    Use basujindal's fork for lower memory usage; Allow using the CPU if no compatible GPU is present; Allow controlling the precision (full or half); An option to save the generated images to the user's home directory (thanks @heylenz for the initial code contribution PR #29)

--- scripts/on_env_start.bat
+++ scripts/on_env_start.bat
@@ -7,7 +7,7 @@
 @if not exist "stable-diffusion\" (
     @set new_install=T
     @echo. & echo "Downloading Stable Diffusion.." & echo.
-    @call git clone https://github.com/CompVis/stable-diffusion.git
+    @call git clone https://github.com/basujindal/stable-diffusion.git
 )
 
 @cd stable-diffusion
@@ -21,6 +21,10 @@
     @echo. & echo "Downloading packages necessary for Stable Diffusion UI.." & echo.
 
     @call conda install -c conda-forge -y --prefix env uvicorn fastapi
+
+    @rem "Check if everything was installed, by running it once more"
+
+    @call conda env update --prefix .\env --file environment.yaml
 ) else (
     @call conda activate .\env
 )
@@ -31,15 +35,9 @@
 )
 
 @if "%new_install%"=="T" (
-    @echo. & echo "Testing your new installation of Stable Diffusion.." & echo.
+    @echo. & echo "Checking your new installation of Stable Diffusion.." & echo.
 
-    python scripts\txt2img.py --prompt "photo of an astronaut riding a motorcycle" --W 256 --H 256 --plms --ckpt sd-v1-4.ckpt --skip_grid --n_samples 1
-
-    @if not exist "outputs\txt2img-samples\samples" (
-        @echo. & echo "There was an error while running Stable Diffusion. Please check the troubleshooting guide (https://github.com/cmdr2/stable-diffusion-ui/wiki/Troubleshooting) for common issues. If that doesn't work, please feel free to file an issue at: https://github.com/cmdr2/stable-diffusion-ui/issues" & echo.
-        @pause
-        @exit /b
-    )
+    @call conda env update --prefix .\env --file environment.yaml
 )
 
 @echo. & echo "Ready to rock!" & echo.
--- ui/sd_internal/runtime.py
+++ ui/sd_internal/runtime.py
@@ -1,63 +1,128 @@
-import argparse, os, sys, glob
-import cv2
+import os, re
 import torch
 import numpy as np
 from omegaconf import OmegaConf
-import PIL
 from PIL import Image
 from tqdm import tqdm, trange
-from imwatermark import WatermarkEncoder
 from itertools import islice
-from einops import rearrange, repeat
-from torchvision.utils import make_grid
+from einops import rearrange
 import time
 from pytorch_lightning import seed_everything
 from torch import autocast
-from contextlib import contextmanager, nullcontext
-
+from contextlib import nullcontext
+from einops import rearrange, repeat
 from ldm.util import instantiate_from_config
-from ldm.models.diffusion.ddim import DDIMSampler
-from ldm.models.diffusion.plms import PLMSSampler
+from optimizedSD.optimUtils import split_weighted_subprompts
+from transformers import logging
 
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
-from transformers import AutoFeatureExtractor
+logging.set_verbosity_error()
 
-# my stuff
+# consts
+config_yaml = "optimizedSD/v1-inference.yaml"
+
+# api stuff
 from . import Request, Response, Image as ResponseImage
 import base64
 from io import BytesIO
 
-
-# load safety model
-safety_model_id = "CompVis/stable-diffusion-safety-checker"
-safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
-safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
-
 # local
+ckpt = None
 model = None
+modelCS = None
+modelFS = None
+model_is_half = False
+model_fs_is_half = False
 device = None
+unet_bs = 1
+precision = 'autocast'
 sampler_plms = None
 sampler_ddim = None
 
 # api
-def load_model(config="configs/stable-diffusion/v1-inference.yaml", ckpt="models/ldm/stable-diffusion-v1/model.ckpt"):
-    global model, device
-    global sampler_plms, sampler_ddim
+def load_model(ckpt_to_use, device_to_use='cuda', turbo=False, unet_bs_to_use=1, precision_to_use='autocast', half_model_fs=False):
+    global ckpt, model, modelCS, modelFS, model_is_half, device, unet_bs, precision, model_fs_is_half
 
-    config = OmegaConf.load(f"{config}")
-    model = load_model_from_config(config, f"{ckpt}")
+    ckpt = ckpt_to_use
+    device = device_to_use
+    precision = precision_to_use
+    unet_bs = unet_bs_to_use
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model = model.to(device)
+    sd = load_model_from_config(f"{ckpt}")
+    li, lo = [], []
+    for key, value in sd.items():
+        sp = key.split(".")
+        if (sp[0]) == "model":
+            if "input_blocks" in sp:
+                li.append(key)
+            elif "middle_block" in sp:
+                li.append(key)
+            elif "time_embed" in sp:
+                li.append(key)
+            else:
+                lo.append(key)
+    for key in li:
+        sd["model1." + key[6:]] = sd.pop(key)
+    for key in lo:
+        sd["model2." + key[6:]] = sd.pop(key)
 
-    sampler_plms = PLMSSampler(model)
-    sampler_ddim = DDIMSampler(model)
+    config = OmegaConf.load(f"{config_yaml}")
+
+    model = instantiate_from_config(config.modelUNet)
+    _, _ = model.load_state_dict(sd, strict=False)
+    model.eval()
+    model.cdevice = device
+    model.unet_bs = unet_bs
+    model.turbo = turbo
+
+    modelCS = instantiate_from_config(config.modelCondStage)
+    _, _ = modelCS.load_state_dict(sd, strict=False)
+    modelCS.eval()
+    modelCS.cond_stage_model.device = device
+
+    modelFS = instantiate_from_config(config.modelFirstStage)
+    _, _ = modelFS.load_state_dict(sd, strict=False)
+    modelFS.eval()
+    del sd
+
+    if device != "cpu" and precision == "autocast":
+        model.half()
+        modelCS.half()
+        model_is_half = True
+    else:
+        model_is_half = False
+
+    if half_model_fs:
+        modelFS.half()
+        model_fs_is_half = True
+    else:
+        model_fs_is_half = False
 
 def mk_img(req: Request):
+    global modelFS, device
+
     res = Response()
     res.images = []
 
-    sampler = sampler_ddim
+    model.turbo = req.turbo
+    if req.use_cpu:
+        device = 'cpu'
+
+        if model_is_half:
+            print('reloading model for cpu')
+            load_model(ckpt, device)
+    else:
+        device = 'cuda'
+
+        if (precision == 'autocast' and (req.use_full_precision or not model_is_half)) or \
+            (precision == 'full' and not req.use_full_precision) or \
+            (req.init_image is None and model_fs_is_half) or \
+            (req.init_image is not None and not model_fs_is_half):
+
+            print('reloading model for cuda')
+            load_model(ckpt, device, model.turbo, unet_bs, ('full' if req.use_full_precision else 'autocast'), half_model_fs=(req.init_image is not None and not req.use_full_precision))
+
+    model.cdevice = device
+    modelCS.cond_stage_model.device = device
 
     opt_prompt = req.prompt
     opt_seed = req.seed
@@ -70,12 +135,12 @@ def mk_img(req: Request):
     opt_f = 8
     opt_ddim_steps = req.num_inference_steps
     opt_ddim_eta = 0.0
-    opt_precision = "autocast"
     opt_strength = req.prompt_strength
-    opt_skip_save = False
+    opt_save_to_disk_path = req.save_to_disk_path
     opt_init_img = req.init_image
+    opt_format = 'png'
 
-    print(opt_prompt, ': seed', opt_seed, 'num_inference_steps', opt_ddim_steps, 'guidance_scale', opt_scale, 'w', opt_W, 'h', opt_H, 'allow_nsfw', req.allow_nsfw)
+    print(req.to_string(), 'device', device)
 
     seed_everything(opt_seed)
 
@@ -83,6 +148,11 @@ def mk_img(req: Request):
     prompt = opt_prompt
     assert prompt is not None
     data = [batch_size * [prompt]]
+
+    if precision == "autocast" and device != "cpu":
+        precision_scope = autocast
+    else:
+        precision_scope = nullcontext
 
     if req.init_image is None:
         handler = _txt2img
@@ -94,87 +164,138 @@ def mk_img(req: Request):
 
         init_image = load_img(req.init_image)
         init_image = init_image.to(device)
+
+        if device != "cpu" and precision == "autocast":
+            init_image = init_image.half()
+
+        modelFS.to(device)
+
         init_image = repeat(init_image, '1 ... -> b ...', b=batch_size)
-        init_latent = model.get_first_stage_encoding(model.encode_first_stage(init_image))  # move to latent space
+        init_latent = modelFS.get_first_stage_encoding(modelFS.encode_first_stage(init_image))  # move to latent space
+
+        if device != "cpu":
+            mem = torch.cuda.memory_allocated() / 1e6
+            modelFS.to("cpu")
+            while torch.cuda.memory_allocated() / 1e6 >= mem:
+                time.sleep(1)
 
         assert 0. <= opt_strength <= 1., 'can only work with strength in [0.0, 1.0]'
         t_enc = int(opt_strength * opt_ddim_steps)
         print(f"target t_enc is {t_enc} steps")
 
-    sampler.make_schedule(ddim_num_steps=opt_ddim_steps, ddim_eta=opt_ddim_eta, verbose=False)
-
-    precision_scope = autocast if opt_precision=="autocast" else nullcontext
+    seeds = ""
     with torch.no_grad():
-        with precision_scope("cuda"):
-            with model.ema_scope():
-                for n in trange(opt_n_iter, desc="Sampling"):
-                    for prompts in tqdm(data, desc="data"):
-                        uc = None
-                        if opt_scale != 1.0:
-                            uc = model.get_learned_conditioning(batch_size * [""])
-                        if isinstance(prompts, tuple):
-                            prompts = list(prompts)
-                        c = model.get_learned_conditioning(prompts)
+        for n in trange(opt_n_iter, desc="Sampling"):
+            for prompts in tqdm(data, desc="data"):
 
-                        # run the handler
-                        if handler == _txt2img:
-                            x_samples = _txt2img(opt_W, opt_H, opt_n_samples, opt_ddim_steps, opt_scale, None, opt_C, opt_f, opt_ddim_eta, c, uc)
-                        else:
-                            x_samples = _img2img(init_latent, t_enc, batch_size, opt_scale, c, uc)
+                if opt_save_to_disk_path is not None:
+                    sample_path = os.path.join(opt_save_to_disk_path, "_".join(re.split(":| ", prompts[0])))[:150]
+                    os.makedirs(sample_path, exist_ok=True)
+                    base_count = len(os.listdir(sample_path))
 
-                        # process the generated images
-                        if req.allow_nsfw:
-                            x_checked_image = x_samples
-                        else:
-                            x_checked_image, has_nsfw_concept = check_safety(x_samples)
+                with precision_scope("cuda"):
+                    modelCS.to(device)
+                    uc = None
+                    if opt_scale != 1.0:
+                        uc = modelCS.get_learned_conditioning(batch_size * [""])
+                    if isinstance(prompts, tuple):
+                        prompts = list(prompts)
 
-                        x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
+                    subprompts, weights = split_weighted_subprompts(prompts[0])
+                    if len(subprompts) > 1:
+                        c = torch.zeros_like(uc)
+                        totalWeight = sum(weights)
+                        # normalize each "sub prompt" and add it
+                        for i in range(len(subprompts)):
+                            weight = weights[i]
+                            # if not skip_normalize:
+                            weight = weight / totalWeight
+                            c = torch.add(c, modelCS.get_learned_conditioning(subprompts[i]), alpha=weight)
+                    else:
+                        c = modelCS.get_learned_conditioning(prompts)
 
-                        if not opt_skip_save:
-                            for i, x_sample in enumerate(x_checked_image_torch):
-                                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                                img = Image.fromarray(x_sample.astype(np.uint8))
-                                img_data = img_to_base64_str(img)
+                    # run the handler
+                    if handler == _txt2img:
+                        x_samples = _txt2img(opt_W, opt_H, opt_n_samples, opt_ddim_steps, opt_scale, None, opt_C, opt_f, opt_ddim_eta, c, uc, opt_seed)
+                    else:
+                        x_samples = _img2img(init_latent, t_enc, batch_size, opt_scale, c, uc, opt_ddim_steps, opt_ddim_eta, opt_seed)
 
-                                res.images.append(ResponseImage(data=img_data))
+                    modelFS.to(device)
+
+                    print("saving images")
+                    for i in range(batch_size):
+
+                        x_samples_ddim = modelFS.decode_first_stage(x_samples[i].unsqueeze(0))
+                        x_sample = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                        x_sample = 255.0 * rearrange(x_sample[0].cpu().numpy(), "c h w -> h w c")
+                        img = Image.fromarray(x_sample.astype(np.uint8))
+
+                        img_data = img_to_base64_str(img)
+                        res.images.append(ResponseImage(data=img_data, seed=opt_seed))
+
+                        if opt_save_to_disk_path is not None:
+                            img.save(
+                                os.path.join(sample_path, "seed_" + str(opt_seed) + "_" + f"{base_count:05}.{opt_format}")
+                            )
+                            base_count += 1
+
+                        seeds += str(opt_seed) + ","
+                        opt_seed += 1
+
+                    if device != "cpu":
+                        mem = torch.cuda.memory_allocated() / 1e6
+                        modelFS.to("cpu")
+                        while torch.cuda.memory_allocated() / 1e6 >= mem:
+                            time.sleep(1)
+                    del x_samples
+                    print("memory_final = ", torch.cuda.memory_allocated() / 1e6)
 
     return res
 
-def _txt2img(opt_W, opt_H, opt_n_samples, opt_ddim_steps, opt_scale, start_code, opt_C, opt_f, opt_ddim_eta, c, uc):
-    sampler = sampler_plms
+def _txt2img(opt_W, opt_H, opt_n_samples, opt_ddim_steps, opt_scale, start_code, opt_C, opt_f, opt_ddim_eta, c, uc, opt_seed):
+    shape = [opt_n_samples, opt_C, opt_H // opt_f, opt_W // opt_f]
 
-    shape = [opt_C, opt_H // opt_f, opt_W // opt_f]
-    samples_ddim, _ = sampler.sample(S=opt_ddim_steps,
-                                        conditioning=c,
-                                        batch_size=opt_n_samples,
-                                        shape=shape,
-                                        verbose=False,
-                                        unconditional_guidance_scale=opt_scale,
-                                        unconditional_conditioning=uc,
-                                        eta=opt_ddim_eta,
-                                        x_T=start_code)
+    if device != "cpu":
+        mem = torch.cuda.memory_allocated() / 1e6
+        modelCS.to("cpu")
+        while torch.cuda.memory_allocated() / 1e6 >= mem:
+            time.sleep(1)
 
-    x_samples_ddim = model.decode_first_stage(samples_ddim)
-    x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-    x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+    samples_ddim = model.sample(
+        S=opt_ddim_steps,
+        conditioning=c,
+        seed=opt_seed,
+        shape=shape,
+        verbose=False,
+        unconditional_guidance_scale=opt_scale,
+        unconditional_conditioning=uc,
+        eta=opt_ddim_eta,
+        x_T=start_code,
+        sampler = 'plms',
+    )
 
-    return x_samples_ddim
+    return samples_ddim
 
-def _img2img(init_latent, t_enc, batch_size, opt_scale, c, uc):
-    sampler = sampler_ddim
-
-    # this part is specific to img2img
+def _img2img(init_latent, t_enc, batch_size, opt_scale, c, uc, opt_ddim_steps, opt_ddim_eta, opt_seed):
     # encode (scaled latent)
-    z_enc = sampler.stochastic_encode(init_latent, torch.tensor([t_enc]*batch_size).to(device))
+    z_enc = model.stochastic_encode(
+        init_latent,
+        torch.tensor([t_enc] * batch_size).to(device),
+        opt_seed,
+        opt_ddim_eta,
+        opt_ddim_steps,
+    )
     # decode it
-    samples = sampler.decode(z_enc, c, t_enc, unconditional_guidance_scale=opt_scale,
-                                unconditional_conditioning=uc,)
+    samples_ddim = model.sample(
+        t_enc,
+        c,
+        z_enc,
+        unconditional_guidance_scale=opt_scale,
+        unconditional_conditioning=uc,
+        sampler = 'ddim'
+    )
 
-    x_samples = model.decode_first_stage(samples)
-    x_samples = torch.clamp((x_samples + 1.0) / 2.0, min=0.0, max=1.0)
-    x_samples = x_samples.cpu().permute(0, 2, 3, 1).numpy() # extra
-
-    return x_samples
+    return samples_ddim
 
 # internal
 
@@ -183,72 +304,22 @@ def chunk(it, size):
     return iter(lambda: tuple(islice(it, size)), ())
 
 
-def numpy_to_pil(images):
-    """
-    Convert a numpy image or a batch of images to a PIL image.
-    """
-    if images.ndim == 3:
-        images = images[None, ...]
-    images = (images * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
-
-    return pil_images
-
-
-def load_model_from_config(config, ckpt, verbose=False):
+def load_model_from_config(ckpt, verbose=False):
     print(f"Loading model from {ckpt}")
     pl_sd = torch.load(ckpt, map_location="cpu")
     if "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
     sd = pl_sd["state_dict"]
-    model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
-    if len(m) > 0 and verbose:
-        print("missing keys:")
-        print(m)
-    if len(u) > 0 and verbose:
-        print("unexpected keys:")
-        print(u)
+    return sd
 
-    model.cuda()
-    model.eval()
-    return model
-
-
-def put_watermark(img, wm_encoder=None):
-    if wm_encoder is not None:
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img = wm_encoder.encode(img, 'dwtDct')
-        img = Image.fromarray(img[:, :, ::-1])
-    return img
-
-
-def load_replacement(x):
-    try:
-        hwc = x.shape
-        y = Image.open("assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
-        y = (np.array(y)/255.0).astype(x.dtype)
-        assert y.shape == x.shape
-        return y
-    except Exception:
-        return x
-
-
-def check_safety(x_image):
-    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
-    assert x_checked_image.shape[0] == len(has_nsfw_concept)
-    for i in range(len(has_nsfw_concept)):
-        if has_nsfw_concept[i]:
-            x_checked_image[i] = load_replacement(x_checked_image[i])
-    return x_checked_image, has_nsfw_concept
+# utils
 
 def load_img(img_str):
     image = base64_str_to_img(img_str).convert("RGB")
     w, h = image.size
     print(f"loaded input image of size ({w}, {h}) from base64")
-    w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
-    image = image.resize((w, h), resample=PIL.Image.LANCZOS)
+    w, h = map(lambda x: x - x % 64, (w, h))  # resize to integer multiple of 64
+    image = image.resize((w, h), resample=Image.LANCZOS)
     image = np.array(image).astype(np.float32) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     image = torch.from_numpy(image)
