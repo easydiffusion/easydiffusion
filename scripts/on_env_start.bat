e8df7177e42f7b036bbaa12a8f46dbe615582279
cmdr2 <secondary.cmdr2@gmail.com>
2022-09-02 15:33:31 +0530

    Another trap for errors

--- scripts/on_env_start.bat
+++ scripts/on_env_start.bat
@@ -2,15 +2,32 @@
 
 @cd ..
 
+@set cmd_had_error=F
+
 @>nul grep -c "sd_git_cloned" scripts\install_status.txt
 @if "%ERRORLEVEL%" EQU "0" (
-    @echo "Stable Diffusion's git repository was already installed"
+    @echo "Stable Diffusion's git repository was already installed. Updating.."
+
+    @cd stable-diffusion
+
+    @call git reset --hard
+    @call git pull
+
+    @cd ..
 ) else (
     @echo. & echo "Downloading Stable Diffusion.." & echo.
 
     @call git clone https://github.com/basujindal/stable-diffusion.git && (
         @echo sd_git_cloned >> scripts\install_status.txt
     ) || (
+        @set cmd_had_error=T
+    )
+
+    if "%ERRORLEVEL" NEQ "0" (
+        @set cmd_had_error=T
+    )
+
+    if "%cmd_had_error"=="T" (
         @echo "Error downloading Stable Diffusion. Please try re-running this installer. If it doesn't work, please copy the messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB or file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues"
         pause
         @exit /b
@@ -30,6 +47,14 @@
     @call conda env create --prefix env -f environment.yaml && (
         @echo conda_sd_env_created >> ..\scripts\install_status.txt
     ) || (
+        @set cmd_had_error=T
+    )
+
+    if "%ERRORLEVEL" NEQ "0" (
+        @set cmd_had_error=T
+    )
+
+    if "%cmd_had_error"=="T" (
         echo "Error installing the packages necessary for Stable Diffusion. Please try re-running this installer. If it doesn't work, please copy the messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB or file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues"
         pause
         exit /b
@@ -47,6 +72,14 @@
     @call conda install -c conda-forge -y --prefix env uvicorn fastapi && (
         @echo conda_sd_ui_deps_installed >> ..\scripts\install_status.txt
     ) || (
+        @set cmd_had_error=T
+    )
+
+    if "%ERRORLEVEL" NEQ "0" (
+        @set cmd_had_error=T
+    )
+
+    if "%cmd_had_error"=="T" (
         echo "Error installing the packages necessary for Stable Diffusion UI. Please try re-running this installer. If it doesn't work, please copy the messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB or file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues"
         pause
         exit /b
