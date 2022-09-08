5170f508f7b86ee0fcc6579b2f6caa6e9c2f0026
cmdr2 <secondary.cmdr2@gmail.com>
2022-09-08 23:18:50 +0530

    Revert "Emergency fix"
    This reverts commit 72900eaf93261b27cc5d150f94fa24ebbab74779.

--- scripts/on_env_start.bat
+++ scripts/on_env_start.bat
@@ -26,14 +26,12 @@
 
     @call git reset --hard
     @call git pull
-    @call git checkout main-fix
-    @call git pull
 
     @cd ..
 ) else (
     @echo. & echo "Downloading Stable Diffusion UI.." & echo.
 
-    @call git clone -b main-fix https://github.com/cmdr2/stable-diffusion-ui.git sd-ui-files && (
+    @call git clone https://github.com/cmdr2/stable-diffusion-ui.git sd-ui-files && (
         @echo sd_ui_git_cloned >> scripts\install_status.txt
     ) || (
         @echo "Error downloading Stable Diffusion UI. Sorry about that, please try to:" & echo "  1. Run this installer again." & echo "  2. If that doesn't fix it, please try the common troubleshooting steps at https://github.com/cmdr2/stable-diffusion-ui/blob/main/Troubleshooting.md" & echo "  3. If those steps don't help, please copy *all* the error messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB" & echo "  4. If that doesn't solve the problem, please file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues" & echo "Thanks!"
