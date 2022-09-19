bea8ae55de42e97012159dfed5ced45268053703
caranicas <kendrick.caranicas@gmail.com>
2022-09-19 10:05:52 -0400

    remove space

--- ui/frontend/build_src/src/App.tsx
+++ ui/frontend/build_src/src/App.tsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 import { ReactLocation, Router } from "@tanstack/react-location";
-
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
 // @ts-ignore
 import { darkTheme, lightTheme } from "./styles/theme/index.css.ts";
-
 import './Translation/config';
-
 const location = new ReactLocation();
 
 function App() {
   // just check for the theme one 1 time
+
   // var { matches } = window.matchMedia('(prefers-color-scheme: dark)')
   const matches = true;
   const themeClass = matches ? darkTheme : lightTheme;
