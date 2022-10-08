66c7b3fcb20f65332c30661e28849637a3c82ed6
cmdr2 <secondary.cmdr2@gmail.com>
2022-10-08 16:47:15 +0530

    Don't use setAttribute to overwrite the style

--- ui/media/main.js
+++ ui/media/main.js
@@ -276,15 +276,15 @@ function resizeInpaintingEditor() {
         widthValue = (widthValue / heightValue) * INPAINTING_EDITOR_SIZE
         heightValue = INPAINTING_EDITOR_SIZE
     }
-    inpaintingEditorContainer.setAttribute("style", `width:${widthValue}px;height:${heightValue}px`)
+
     inpaintingEditorContainer.style.width = widthValue + 'px'
     inpaintingEditorContainer.style.height = heightValue + 'px'
     inpaintingEditor.opts.enlargeYourContainer = true
 
     inpaintingEditor.resize()
 
-    inpaintingEditor.ctx.lineCap = "round";
-    inpaintingEditor.ctx.lineJoin = "round";
+    inpaintingEditor.ctx.lineCap = "round"
+    inpaintingEditor.ctx.lineJoin = "round"
     inpaintingEditor.ctx.lineWidth = inpaintingEditor.opts.size
     inpaintingEditor.setColor(inpaintingEditor.opts.color)
 }
