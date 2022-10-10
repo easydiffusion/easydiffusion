a6913dfe2950fd7ef96b6b3f494e9766e87eb7e1
cmdr2 <secondary.cmdr2@gmail.com>
2022-10-10 19:38:41 +0530

    Rename dev console scripts

--- Developer Console.cmd
+++ Developer Console.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+echo "Opening Stable Diffusion UI - Developer Console.." & echo.
+
 set SD_BASE_DIR=%cd%
 set MAMBA_ROOT_PREFIX=%SD_BASE_DIR%\env\mamba
 set INSTALL_ENV_DIR=%SD_BASE_DIR%\env\installer_env
--- developer_console.sh
+++ developer_console.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ "$0" == "bash" ]; then
+  echo "Opening Stable Diffusion UI - Developer Console.."
+  echo ""
+
   export SD_BASE_DIR=`pwd`
   export MAMBA_ROOT_PREFIX="$SD_BASE_DIR/env/mamba"
   export INSTALL_ENV_DIR="$SD_BASE_DIR/env/installer_env"
@@ -9,5 +12,5 @@ if [ "$0" == "bash" ]; then
 
   micromamba activate "$INSTALL_ENV_DIR"
 else
-  bash --init-file open_dev_console.sh
+  bash --init-file developer_console.sh
 fi
