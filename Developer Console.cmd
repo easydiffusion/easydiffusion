e521b350caeed507de57079a64bec7278b06cf29
cmdr2 <secondary.cmdr2@gmail.com>
2022-10-10 20:06:26 +0530

    Activate the project env dir in the dev console

--- Developer Console.cmd
+++ Developer Console.cmd
@@ -5,9 +5,11 @@ echo "Opening Stable Diffusion UI - Developer Console.." & echo.
 set SD_BASE_DIR=%cd%
 set MAMBA_ROOT_PREFIX=%SD_BASE_DIR%\env\mamba
 set INSTALL_ENV_DIR=%SD_BASE_DIR%\env\installer_env
+set PROJECT_ENV_DIR=%SD_BASE_DIR%\env\project_env
 
 call "%MAMBA_ROOT_PREFIX%\condabin\mamba_hook.bat"
 
 call micromamba activate "%INSTALL_ENV_DIR%"
+call micromamba activate "%PROJECT_ENV_DIR%"
 
 cmd /k
--- developer_console.sh
+++ developer_console.sh
@@ -7,10 +7,12 @@ if [ "$0" == "bash" ]; then
   export SD_BASE_DIR=`pwd`
   export MAMBA_ROOT_PREFIX="$SD_BASE_DIR/env/mamba"
   export INSTALL_ENV_DIR="$SD_BASE_DIR/env/installer_env"
+  export PROJECT_ENV_DIR="$SD_BASE_DIR/env/project_env"
 
   eval "$($MAMBA_ROOT_PREFIX/micromamba shell hook -s posix)"
 
   micromamba activate "$INSTALL_ENV_DIR"
+  micromamba activate "$PROJECT_ENV_DIR"
 else
   bash --init-file developer_console.sh
 fi
