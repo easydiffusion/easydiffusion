a42f3e3e235b62008e3d2ed915c86fac741f693d
patriceac <48073125+patriceac@users.noreply.github.com>
2022-11-27 00:11:16 -0800

    Fix tag matching

--- ui/media/js/image-modifiers.js
+++ ui/media/js/image-modifiers.js
@@ -219,7 +219,7 @@ function refreshTagsList() {
         editorModifierTagsList.appendChild(tag.element)
 
         tag.element.addEventListener('click', () => {
-            let idx = activeTags.indexOf(tag)
+            let idx = activeTags.findIndex(o => { return o.name === tag.name })            
 
             if (idx !== -1 && activeTags[idx].originElement !== undefined) {
                 activeTags[idx].originElement.classList.remove(activeCardClass)
