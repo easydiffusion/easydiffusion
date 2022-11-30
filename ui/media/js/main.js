cca896f64e6b52b86a0173388a26746507be75f0
Marc-Andre Ferland <madrang@gmail.com>
2022-11-30 11:24:22 -0500

    Allow getPrompts to be reused be plugins.

--- ui/media/js/main.js
+++ ui/media/js/main.js
@@ -469,10 +469,9 @@ function makeImage() {
         guidanceScaleField.value = guidanceScaleSlider.value / 10
     }
     const taskTemplate = getCurrentUserRequest()
-    const newTaskRequests = []
-    getPrompts().forEach((prompt) => newTaskRequests.push(Object.assign({}, taskTemplate, {
+    const newTaskRequests = getPrompts().map((prompt) => Object.assign({}, taskTemplate, {
         reqBody: Object.assign({ prompt: prompt }, taskTemplate.reqBody)
-    })))
+    }))
     newTaskRequests.forEach(createTask)
 
     initialText.style.display = 'none'
@@ -893,8 +892,10 @@ function getCurrentUserRequest() {
     return newTask
 }
 
-function getPrompts() {
-    let prompts = promptField.value
+function getPrompts(prompts) {
+    if (typeof prompts === 'undefined') {
+        prompts = promptField.value
+    }
     if (prompts.trim() === '') {
         return ['']
     }
