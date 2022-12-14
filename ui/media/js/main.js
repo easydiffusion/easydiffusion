fb075a0013d476120ce79f4eb2d9ed19132f75d0
cmdr2 <secondary.cmdr2@gmail.com>
2022-12-14 16:53:50 +0530

    Fix whitespace

--- ui/media/js/main.js
+++ ui/media/js/main.js
@@ -629,9 +629,9 @@ function onTaskCompleted(task, reqBody, instance, outputContainer, stepUpdate) {
     time /= 1000
 
     if (task.batchesDone == task.batchCount) {
-	if (!task.outputMsg.innerText.toLowerCase().includes('error')) {
+        if (!task.outputMsg.innerText.toLowerCase().includes('error')) {
             task.outputMsg.innerText = `Processed ${task.numOutputsTotal} images in ${time} seconds`
-	}
+        }
         task.progressBar.style.height = "0px"
         task.progressBar.style.border = "0px solid var(--background-color3)"
         task.progressBar.classList.remove("active")
