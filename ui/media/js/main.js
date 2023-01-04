567c02bf5d954edb00393a508ff92887929f74ba
AssassinJN <AssassinJN@users.noreply.github.com>
2023-01-04 10:04:52 -0500

    return taskEntry.id on createTask
    I would like to have createTask return the taskEntry.id in order to allow for watchers or callbacks to be able to reference tasks by id more easily.

--- ui/media/js/main.js
+++ ui/media/js/main.js
@@ -906,7 +906,7 @@ function createTask(task) {
     if (task.previewPrompt.innerText.trim() === '') {
         task.previewPrompt.innerHTML = '&nbsp;' // allows the results to be collapsed
     }
-
+    return taskEntry.id
 }
 
 function getCurrentUserRequest() {
