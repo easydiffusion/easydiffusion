1429a44f0e5790aecc38b1f5dfecec693367ebcf
patriceac <48073125+patriceac@users.noreply.github.com>
2023-04-01 00:00:19 -0700

    Align source code formatting

--- ui/media/js/image-editor.js
+++ ui/media/js/image-editor.js
@@ -672,17 +672,17 @@ class ImageEditor {
 				else {
 					this.history.redo()
 				}
-			        event.stopPropagation();
+				event.stopPropagation();
 				event.preventDefault();
 			}
 			if (event.key == "y" && event.ctrlKey) {
 				this.history.redo()
-			        event.stopPropagation();
+				event.stopPropagation();
 				event.preventDefault();
 			}
 			if (event.key === "Escape") {
 				this.hide()
-			        event.stopPropagation();
+				event.stopPropagation();
 				event.preventDefault();
 			}
 		}
