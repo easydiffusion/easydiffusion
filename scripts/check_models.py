d0f4476ba589f6e9bc9e94478558b15a15db0640
cmdr2 <secondary.cmdr2@gmail.com>
2023-04-20 16:22:42 +0530

    Suggest downloading a model downloading in the troubleshooting steps. Thanks JeLuf

--- scripts/check_models.py
+++ scripts/check_models.py
@@ -88,7 +88,7 @@ def fail(model_name):
     print(
         f"""Error downloading the {model_name} model. Sorry about that, please try to:
 1. Run this installer again.
-2. If that doesn't fix it, please try the common troubleshooting steps at https://github.com/cmdr2/stable-diffusion-ui/wiki/Troubleshooting
+2. If that doesn't fix it, please try to download the file manually. The address to download from, and the destination to save to are printed above this message.
 3. If those steps don't help, please copy *all* the error messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB
 4. If that doesn't solve the problem, please file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues
 Thanks!"""
