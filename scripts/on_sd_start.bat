3ae851ab1f59908f12498fa1fc8503f459bd5d19
cmdr2 <shashank.shekhar.global@gmail.com>
2023-04-24 14:32:18 +0530

    Revert "Revert "Stop messing with %USERPROFILE%""

--- scripts/on_sd_start.bat
+++ scripts/on_sd_start.bat
@@ -8,7 +8,7 @@
 @copy sd-ui-files\scripts\check_models.py scripts\ /Y
 
 if exist "%cd%\profile" (
-    set USERPROFILE=%cd%\profile
+    set HF_HOME=%cd%\profile\.cache\huggingface
 )
 
 @rem set the correct installer path (current vs legacy)
