812fc20c39a248d90b8d9d6865fc4203a43b8989
cmdr2 <secondary.cmdr2@gmail.com>
2023-07-16 12:58:01 +0530

    sdkit 1.0.131 - Fix loras ignoring several layers

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -18,7 +18,7 @@ os_name = platform.system()
 modules_to_check = {
     "torch": ("1.11.0", "1.13.1", "2.0.0"),
     "torchvision": ("0.12.0", "0.14.1", "0.15.1"),
-    "sdkit": "1.0.130",
+    "sdkit": "1.0.131",
     "stable-diffusion-sdkit": "2.1.4",
     "rich": "12.6.0",
     "uvicorn": "0.19.0",
