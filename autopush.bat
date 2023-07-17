565310f6fde67fbf4e7372d72b9666358e800f54
lpjhelder <lpjhelder@gmail.com>
2023-07-17 17:27:07 -0300

    test1

--- autopush.bat
+++ autopush.bat
@@ -4,11 +4,10 @@ set /p commit_message="Enter a commit message: "
 echo.
 echo Committing with message: "%commit_message%"
 
+git pull
 git add .
 git commit -m "%commit_message%"
-
-echo.
-echo Pushing to remote...
+git push
 
 echo.
 echo Done!
