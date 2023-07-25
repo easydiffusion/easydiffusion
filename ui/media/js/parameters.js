7b5e2b4a1217a100126b98c0cef397eab90e598f
JeLuF <jf@mormo.org>
2023-07-25 04:09:30 +0200

    Display Test Diffusers checkbox when enabling beta (#1430)
    * Display Test Diffusers checkbox when enabling beta
    
    * remove debug

--- ui/media/js/parameters.js
+++ ui/media/js/parameters.js
@@ -409,7 +409,7 @@ async function getAppConfig() {
             useBetaChannelField.checked = true
             document.querySelector("#updateBranchLabel").innerText = "(beta)"
         } else {
-            getParameterSettingsEntry("test_diffusers").style.display = "none"
+            getParameterSettingsEntry("test_diffusers").classList.add("displayNone")
         }
         if (config.ui && config.ui.open_browser_on_start === false) {
             uiOpenBrowserOnStartField.checked = false
@@ -743,3 +743,11 @@ navigator.permissions.query({ name: "clipboard-write" }).then(function(result) {
 })
 
 document.addEventListener("system_info_update", (e) => setDeviceInfo(e.detail))
+
+useBetaChannelField.addEventListener('change', (e) => {
+    if (e.target.checked) { 
+        getParameterSettingsEntry("test_diffusers").classList.remove('displayNone') 
+    } else { 
+        getParameterSettingsEntry("test_diffusers").classList.add('displayNone')
+    }
+})
