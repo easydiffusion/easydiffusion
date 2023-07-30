92009cff2400255a7b5e0d47553c8fbafe621b3c
ManInDark <61268856+ManInDark@users.noreply.github.com>
2023-07-30 22:45:52 +0200

    Yanked files from https://github.com/JeLuF/stable-diffusion-ui/tree/bucketlite/ui/easydiffusion/easydb to develop

---
+++ ui/easydiffusion/easydb/database.py
@@ -0,0 +1,15 @@
+import os
+from easydiffusion import app
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+os.makedirs(app.BUCKET_DIR, exist_ok=True)
+SQLALCHEMY_DATABASE_URL = "sqlite:///"+os.path.join(app.BUCKET_DIR, "bucket.db")
+print("## SQLALCHEMY_DATABASE_URL = ", SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+BucketBase = declarative_base()
