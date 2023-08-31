73b3beb81b349be5b93d271f6007af80c8c918c6
Ben Zichettello <bz@projectample.com>
2023-08-31 13:19:23 -0400

    feature: add a label to the task item html that indicates number of images enqueued (prior, it was unclear before task begins processing)

--- ui/media/js/main.js
+++ ui/media/js/main.js
@@ -1102,6 +1102,7 @@ function createTask(task) {
     taskEntry.className = "imageTaskContainer"
     taskEntry.innerHTML = ` <div class="header-content panel collapsible active">
                                 <i class="drag-handle fa-solid fa-grip"></i>
+                                ${task.numOutputsTotal > 1 ? `<div class="taskNumberOfImagesLabel">✕${task.numOutputsTotal}</div>` : ''}
                                 <div class="taskStatusLabel">Enqueued</div>
                                 <button class="secondaryButton stopTask"><i class="fa-solid fa-xmark"></i> Cancel</button>
                                 <button class="tertiaryButton useSettings"><i class="fa-solid fa-redo"></i> Use these settings</button>
