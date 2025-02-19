7a383d7bc48c0111724fcd2aa735701ee5d4aeae
cmdr2 <secondary.cmdr2@gmail.com>
2025-02-19 14:56:23 +0530

    sdkit upgrade - fixes loras with numpy arrays

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -97,10 +97,10 @@ def update_modules():
     #  if sdkit is 2.0.15.x (or lower), then diffusers should be restricted to 0.21.4 (see below for the reason)
     #  otherwise use the current sdkit version (with the corresponding diffusers version)
 
-    expected_sdkit_version_str = "2.0.22.4"
+    expected_sdkit_version_str = "2.0.22.6"
     expected_diffusers_version_str = "0.28.2"
 
-    legacy_sdkit_version_str = "2.0.15.13"
+    legacy_sdkit_version_str = "2.0.15.15"
     legacy_diffusers_version_str = "0.21.4"
 
     sdkit_version_str = version("sdkit")
