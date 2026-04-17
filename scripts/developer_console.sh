bd6e26e66e415ab8dc39d8eefc65c7f90e8c9223
jdumke <jd@bepe.de>
2026-04-17 09:55:40 +0200

    Removed references to Python 3.8 in unix startup files. (#1975)
    * Removed references to Python 3.8 in unix startup files.
    on_env_start.sh and developer_console.sh still exported PYTHONPATH for python 3.8, which is outdated, replaced with a more flexible approach. Also altered the shebangs to #!/usr/bin/env bash.
    TODO: fix the same issue in Windows startup files, I can't do ist, I havn't a testing environment for MS.
    
    * Use sys.version_info instead of shell parsing
    
    * Use sys.version_info instead of shell parsing
    
    * the oldest legacy env will stay fixed at python 3.8
    
    * Bring back the oldest legacy env's PYTHONPATH
    
    * Update developer_console.sh
    
    * Update env names
    
    ---------
    
    Co-authored-by: cmdr2 <secondary.cmdr2@gmail.com>

--- scripts/developer_console.sh
+++ scripts/developer_console.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/bin/env bash
+#setup enviroment
+if [ -e "installer_files/env" ]; then
+	export ENVFOLDER="$(pwd)/installer_files/env"
+	export PATH="${ENVFOLDER}/bin:$PATH"; 
+	# check python version und adjust PYTHONPATH
+	if [ -e "${ENVFOLDER}/bin/python"]; then
+		export PYTHONVERSION="$(${ENVFOLDER}/bin/python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
+		export PYTHONPATH="${ENVFOLDER}/lib/python${PATHONVERSION}/site-packages"
+	fi
+fi
+
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
@@ -8,7 +19,6 @@ if [ "$0" == "bash" ]; then
 
   # set legacy and new installer's PATH, if they exist
   if [ -e "installer" ]; then export PATH="$(pwd)/installer/bin:$PATH"; fi
-  if [ -e "installer_files/env" ]; then export PATH="$(pwd)/installer_files/env/bin:$PATH"; fi
 
   # activate the installer env
   CONDA_BASEPATH=$(conda info --base)
@@ -26,10 +36,7 @@ if [ "$0" == "bash" ]; then
 
   echo ""
 
-  # activate the legacy environment (if present) and set PYTHONPATH
-  if [ -e "installer_files/env" ]; then
-    export PYTHONPATH="$(pwd)/installer_files/env/lib/python3.8/site-packages"
-  fi
+  # activate the legacy environment (if present) 
   if [ -e "stable-diffusion/env" ]; then
     CONDA_BASEPATH=$(conda info --base)
     source "$CONDA_BASEPATH/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
