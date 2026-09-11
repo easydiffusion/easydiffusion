8a8a68d888cb7322d06987c1d66ff3a271215ed8
Bryce V <bryce.christian@gmail.com>
2026-09-11 02:46:50 -0400

    Fix missing ensure_torchruntime.py to startup scripts

--- scripts/on_env_start.sh
+++ scripts/on_env_start.sh
@@ -70,6 +70,7 @@ cp sd-ui-files/scripts/check_modules.py scripts/
 cp sd-ui-files/scripts/get_config.py scripts/
 cp sd-ui-files/scripts/config.yaml.sample scripts/
 cp sd-ui-files/scripts/webui_console.py scripts/
+cp sd-ui-files/scripts/ensure_torchruntime.py scripts/
 cp sd-ui-files/scripts/start.sh .
 cp sd-ui-files/scripts/developer_console.sh .
 cp sd-ui-files/scripts/functions.sh scripts/
--- scripts/on_sd_start.sh
+++ scripts/on_sd_start.sh
@@ -7,6 +7,7 @@ cp sd-ui-files/scripts/check_modules.py scripts/
 cp sd-ui-files/scripts/get_config.py scripts/
 cp sd-ui-files/scripts/config.yaml.sample scripts/
 cp sd-ui-files/scripts/webui_console.py scripts/
+cp sd-ui-files/scripts/ensure_torchruntime.py scripts/
 
 
 source ./scripts/functions.sh
@@ -50,10 +51,11 @@ fi
 if [ -e "src" ]; then mv src src-old; fi
 if [ -e "ldm" ]; then mv ldm ldm-old; fi
 
+cd ..
+
 # this is outside check_modules.py to ensure that the required version of torchruntime is present (installing if necessary)
 python scripts/ensure_torchruntime.py
 
-cd ..
 # Download the required packages
 python scripts/check_modules.py --launch-uvicorn
 
